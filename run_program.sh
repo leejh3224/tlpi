3359aa5a4d7093fa3591f7dbfe1102e33c568ce7
gompro <junhyung3224@gmail.com>
2021-08-01 16:19:44 +0900

    fix: make sapce seperated argument work as expected

--- run_program.sh
+++ run_program.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-./exe/out $ARG0 $ARG1 $ARG2 $ARG3
+if [ -z "$ARG0" ]; then
+    ./exe/out
+elif [ -z "$ARG1" ]; then
+    ./exe/out "$ARG0"
+elif [ -z "$ARG2" ]; then
+    ./exe/out "$ARG0" "$ARG1"
+elif [ -z "$ARG3" ]; then
+    ./exe/out "$ARG0" "$ARG1" "$ARG2"
+else
+    ./exe/out "$ARG0" "$ARG1" "$ARG2" "$ARG3"
+fi
